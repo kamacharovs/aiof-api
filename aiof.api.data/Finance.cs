21fe107484376f01f49dc8a49ee6e052c94fe97f
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-28 15:59:20 -0400

    add finance

---
+++ aiof.api.data/Finance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aiof.api.data
+{
+    public class Finance : IPublicKeyId
+    {
+        public int Id { get; set; }
+        public Guid PublicKey { get; set; }
+        public Guid UserId { get; set; }
+
+        public List<Asset> Assets { get; set; } = new List<Asset>();
+        public List<Liability> Liabilities { get; set; } = new List<Liability>();
+        public List<Goal> Goals { get; set; } = new List<Goal>();
+    }
+}
