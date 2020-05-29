afc103a376b29e253a987f7c23e4f0f6afd9ae76
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-29 09:25:39 -0400

    add asset type

---
+++ aiof.api.data/AssetType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aiof.api.data
+{
+    public class AssetType : IAssetType
+    {
+        public string Name { get; set; }
+    }
+}
