d7590a172a852405386073e5540294bde3e860e1
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-29 08:11:00 -0400

    add unit tests for aiof repository

--- aiof.api.tests/AiofRepository.Tests.cs
+++ aiof.api.tests/AiofRepository.Tests.cs
@@ -1,9 +1,13 @@
 using System;
+
 using Xunit;
+
+using aiof.api.data;
+using aiof.api.services;
 
 namespace aiof.api.tests
 {
-    public class UnitTest1
+    public class AiofRepositoryTests
     {
         [Fact]
         public void Test1()
