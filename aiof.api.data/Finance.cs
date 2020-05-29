09e7ad0103b62e6c9cefbc845ad12170b90ca7b6
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-29 08:19:57 -0400

    update finance with interface

--- aiof.api.data/Finance.cs
+++ aiof.api.data/Finance.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace aiof.api.data
 {
-    public class Finance : IPublicKeyId
+    public class Finance : IFinance, IPublicKeyId
     {
         public int Id { get; set; }
         public Guid PublicKey { get; set; }
---
+++ aiof.api.data/IFinance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aiof.api.data
+{
+    public interface IFinance
+    {
+        int Id { get; set; }
+        Guid PublicKey { get; set; }
+        int UserId { get; set; }
+        User User { get; set; }
+        List<Asset> Assets { get; set; }
+        List<Liability> Liabilities { get; set; }
+        List<Goal> Goals { get; set; }
+    }
+}
