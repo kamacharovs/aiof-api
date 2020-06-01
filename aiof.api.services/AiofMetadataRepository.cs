4868ed991f4a0f91ad663325aa03a9ceea2a2e26
Georgi Kamacharov <gkamacharov@gmail.com>
2020-06-01 15:43:37 -0400

    add aiof metadata repository

---
+++ aiof.api.services/AiofMetadataRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+using Microsoft.Extensions.Logging;
+
+using aiof.api.data;
+
+namespace aiof.api.services
+{
+    public class AiofMetadataRepository : IAiofMetadataRepository
+    {
+        private readonly HttpClient _client;
+        private readonly ILogger<AiofMetadataRepository> _logger;
+
+        public AiofMetadataRepository(HttpClient client, ILogger<AiofMetadataRepository> logger)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task SendMetadataAsync()
+        {
+            var response = await _client.GetStringAsync("frequencies");
+            var k = 0;
+        }
+    }
+}
---
+++ aiof.api.services/IAiofMetadataRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aiof.api.services
+{
+    public interface IAiofMetadataRepository
+    {
+        Task SendMetadataAsync();
+    }
+}
