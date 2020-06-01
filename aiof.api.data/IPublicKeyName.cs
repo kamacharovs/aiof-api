5b81a85a1ed0c99b138e4198cfa1f69fb52b57ee
Georgi Kamacharov <gkamacharov@gmail.com>
2020-06-01 10:49:27 -0400

    add public key name interface

---
+++ aiof.api.data/IPublicKeyName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aiof.api.data
+{
+    public interface IPublicKeyName
+    {
+        string Name { get; set; }
+        Guid PublicKey { get; set; }
+    }
+}
