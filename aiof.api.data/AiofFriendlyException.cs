979f2dfb1705ee4bf70df411e92a743772b0f471
Georgi Kamacharov <gkamacharov@gmail.com>
2020-06-02 08:33:33 -0400

    add friendly exception

---
+++ aiof.api.data/AiofFriendlyException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace aiof.api.data
+{
+    public class AiofFriendlyException : AiofException
+    {
+        public AiofFriendlyException()
+        { }
+
+        public AiofFriendlyException(string message)
+            : base(message)
+        { }
+
+        public AiofFriendlyException(HttpStatusCode statusCode, string message)
+            : base(statusCode, message)
+        { }
+
+        public AiofFriendlyException(string message, Exception inner)
+            : base(message, inner)
+        { }
+    }
+}
