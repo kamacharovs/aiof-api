11bfb90d820ef01d174bcf054a27b3cae8d2db60
Georgi Kamacharov <gkamacharov@gmail.com>
2020-06-03 10:25:06 -0400

    add test controller to throw 500 and 400

---
+++ aiof.api.core/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using aiof.api.services;
+using aiof.api.data;
+
+namespace aiof.api.core.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("throw/500")]
+        public void Throw500()
+        {
+            throw new AiofFriendlyException("test 500");
+        }
+
+        [HttpGet]
+        [Route("throw/400")]
+        public void Throw400()
+        {
+            throw new AiofFriendlyException(HttpStatusCode.BadRequest, "test 400");
+        }
+    }
+}
