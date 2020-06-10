56df1c7180aa0802d7817abbdfe49410839f8558
Georgi Kamacharov <gkamacharov@gmail.com>
2020-06-10 09:00:30 -0400

    add auto mapping profile

---
+++ aiof.api.data/AutoMappingProfile.Dto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AutoMapper;
+
+namespace aiof.api.data
+{
+    public class AutoMappingProfileDto : Profile
+    {
+        public AutoMappingProfileDto()
+        {
+            CreateMap<Asset, AssetDto>()
+                .ForMember(d => d.Name, o => o.MapFrom(s => s.Name))
+                .ForMember(d => d.TypeName, o => o.MapFrom(s => s.TypeName))
+                .ForMember(d => d.Name, o => o.MapFrom(s => s.Value))
+                .ForMember(d => d.Name, o => o.MapFrom(s => s.FinanceId));
+        }
+    }
+}
