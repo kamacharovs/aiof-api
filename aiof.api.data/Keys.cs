70ffc1ba6dd3786e6a6ca48ac15735a79d7a6904
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-30 09:49:58 -0400

    add Keys and update settings

---
+++ aiof.api.data/Keys.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace aiof.api.data
+{
+    public static class Keys
+    {
+        public const string Database = nameof(Database);
+
+        public const string Metadata = nameof(Metadata);
+        public const string BaseUrl = nameof(BaseUrl);
+        
+        public const string OpenApi = nameof(OpenApi);
+        public const string Version = nameof(Version);
+        public const string Title = nameof(Title);
+        public const string Description = nameof(Description);
+        public const string Contact = nameof(Contact);
+        public const string Name = nameof(Name);
+        public const string Email = nameof(Email);
+        public const string Url = nameof(Url);
+        
+        public const string License = nameof(License);
+    }
+}
