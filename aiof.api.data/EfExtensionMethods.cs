f024bd225385d16790ba0febe28155a6c978cc3f
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-02 08:54:52 -0400

    add update goal exntesion method

--- aiof.api.data/EfExtensionMethods.cs
+++ aiof.api.data/EfExtensionMethods.cs
@@ -1,14 +1,35 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace aiof.api.data
 {
     public static class EfExtensionMethods
     {
-        public static async Task<Goal> UpdateGoalAsync(this Goal goal, 
-            AiofContext context, 
+        public static async Task<Goal> UpdateGoalAsync(this AiofContext context, 
+            Goal goal, 
             string name)
         {
+            goal.Name = name;
+
+            context.Goals
+                .Update(goal);
+
+            await context.SaveChangesAsync();
+
+            return goal;
+        }
+
+        public static async Task<Goal> UpdateGoalAsync(this AiofContext context, 
+            int id,
+            string name)
+        {
+            var goal = await context.Goals
+                .FirstOrDefaultAsync(x => x.Id == id)
+                ?? throw new AiofNotFoundException();
+
             goal.Name = name;
 
             context.Goals
