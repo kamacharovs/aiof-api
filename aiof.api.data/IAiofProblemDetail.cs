8fd6fd09c994eac63e8c2596f1d52a9788c8c104
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-11 07:26:14 -0400

    add aiof problem detail

---
+++ aiof.api.data/IAiofProblemDetail.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace aiof.api.data
+{
+    public interface IAiofProblemDetail
+    {
+        [JsonPropertyName("code")]
+        [Required]
+        int? Code { get; set; }
+        
+        [JsonPropertyName("message")]
+        [Required]
+        string Message { get; set; }
+
+        [JsonPropertyName("traceId")]
+        [Required]
+        string TraceId { get; set; }
+
+        [JsonPropertyName("errors")]
+        IEnumerable<string> Errors { get; set; }
+    }
+}
