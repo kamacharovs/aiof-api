184ff43060037b0593480960a6d28fe20cb409b8
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-12 13:51:40 -0400

    add utils

---
+++ aiof.api.data/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace aiof.api.data
+{
+    public static class Utils
+    {
+        public static string ToSnakeCase(this string str)
+        {
+            return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+        }
+
+        public static PropertyBuilder HasSnakeCaseColumnName(this PropertyBuilder propertyBuilder)
+        {
+            propertyBuilder.Metadata.SetColumnName(
+                propertyBuilder
+                    .Metadata
+                    .Name
+                    .ToSnakeCase());
+
+            return propertyBuilder;
+        }
+    }
+}
