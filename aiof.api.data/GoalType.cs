34ff59005d2dd8f38969b617df832fc65d25f798
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-12 15:33:02 -0400

    update goal validator for frequency and amount

--- aiof.api.data/GoalType.cs
+++ aiof.api.data/GoalType.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace aiof.api.data
 {
     public class GoalType : IGoalType, 
         IPublicKeyName
     {
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
+
+        [Required]
         public Guid PublicKey { get; set; } = Guid.NewGuid();
     }
 }
--- aiof.api.data/IGoalType.cs
+++ aiof.api.data/IGoalType.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace aiof.api.data
 {
     public interface IGoalType
     {
+        [Required]
+        [MaxLength(100)]
         string Name { get; set; }
+
+        [Required]
         Guid PublicKey { get; set; }
     }
 }
