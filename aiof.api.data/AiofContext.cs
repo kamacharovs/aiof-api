9bbf9154c39a4e49f9a0d2c4a58cfab5c8244e52
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-27 16:43:21 -0400

    add subscription

--- aiof.api.data/AiofContext.cs
+++ aiof.api.data/AiofContext.cs
@@ -17,6 +17,7 @@ namespace aiof.api.data
         public virtual DbSet<LiabilityType> LiabilityTypes { get; set; }
         public virtual DbSet<GoalType> GoalTypes { get; set; }
         public virtual DbSet<Frequency> Frequencies { get; set; }
+        public virtual DbSet<Subscription> Subscriptions { get; set; }
 
         public AiofContext(DbContextOptions<AiofContext> options)
             : base(options)
@@ -196,6 +197,27 @@ namespace aiof.api.data
                 e.Property(x => x.Name).HasSnakeCaseColumnName().HasMaxLength(20).IsRequired();
                 e.Property(x => x.PublicKey).HasSnakeCaseColumnName().IsRequired();
                 e.Property(x => x.Value).HasSnakeCaseColumnName().IsRequired();
+            });
+
+            modelBuilder.Entity<Subscription>(e =>
+            {
+                e.ToTable("subscription");
+
+                e.HasKey(x => x.Id);
+
+                e.Property(x => x.Id).HasSnakeCaseColumnName().ValueGeneratedOnAdd().IsRequired();
+                e.Property(x => x.PublicKey).HasSnakeCaseColumnName().IsRequired();
+                e.Property(x => x.Name).HasSnakeCaseColumnName().HasMaxLength(200).IsRequired();
+                e.Property(x => x.Amount).HasSnakeCaseColumnName().IsRequired();
+                e.Property(x => x.FrequencyName).HasSnakeCaseColumnName().HasMaxLength(20).IsRequired();
+                e.Property(x => x.From).HasSnakeCaseColumnName().HasMaxLength(200);
+                e.Property(x => x.Url).HasSnakeCaseColumnName().HasMaxLength(500);
+                e.Property(x => x.UserId).HasSnakeCaseColumnName().IsRequired();
+
+                e.HasOne(x => x.Frequency)
+                    .WithMany()
+                    .HasForeignKey(x => x.FrequencyName)
+                    .IsRequired();
             });
         }
     }
---
+++ aiof.api.data/Subscription.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace aiof.api.data
+{
+    public class Subscription :
+        IPublicKeyId, IPublicKeyName
+    {
+        [JsonIgnore]
+        [Required]
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        [Required]
+        public Guid PublicKey { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string FrequencyName { get; set; }
+
+        [JsonIgnore]
+        public Frequency Frequency { get; set; }
+
+        [MaxLength(200)]
+        public string From { get; set; }
+
+        [MaxLength(500)]
+        public string Url { get; set; }
+
+        [JsonIgnore]
+        [Required]
+        public int UserId { get; set; }
+    }
+}
