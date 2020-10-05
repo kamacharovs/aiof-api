7d339c2c6d86094e8a28a74210366236a463c374
Georgi Kamacharov <gkamacharov@gmail.com>
2020-10-05 16:51:05 -0400

    add add_isdeleted migration

---
+++ aiof.api.data/Migrations/20201005204757_add_isdeleted.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace aiof.api.data.Migrations
+{
+    public partial class add_isdeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "subscription",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "liability",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "goal",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "asset",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "account",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "subscription");
+
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "liability");
+
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "goal");
+
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "asset");
+
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "account");
+        }
+    }
+}
