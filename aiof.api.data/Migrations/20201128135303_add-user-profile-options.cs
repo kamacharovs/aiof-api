380af7f9890da1d2392c8437c9c14409d5e0a0a7
Georgi Kamacharov <gkamacharov@gmail.com>
2020-11-28 08:55:17 -0500

    add add-user-profile-options migration

---
+++ aiof.api.data/Migrations/20201128135303_add-user-profile-options.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace aiof.api.data.Migrations
+{
+    public partial class adduserprofileoptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "gender",
+                columns: table => new
+                {
+                    name = table.Column<string>(maxLength: 100, nullable: false),
+                    public_key = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_gender", x => x.name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "household_adult",
+                columns: table => new
+                {
+                    name = table.Column<string>(maxLength: 100, nullable: false),
+                    public_key = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_household_adult", x => x.name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "household_child",
+                columns: table => new
+                {
+                    name = table.Column<string>(maxLength: 100, nullable: false),
+                    public_key = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_household_child", x => x.name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "marital_status",
+                columns: table => new
+                {
+                    name = table.Column<string>(maxLength: 100, nullable: false),
+                    public_key = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_marital_status", x => x.name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "residential_status",
+                columns: table => new
+                {
+                    name = table.Column<string>(maxLength: 100, nullable: false),
+                    public_key = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_residential_status", x => x.name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "gender");
+
+            migrationBuilder.DropTable(
+                name: "household_adult");
+
+            migrationBuilder.DropTable(
+                name: "household_child");
+
+            migrationBuilder.DropTable(
+                name: "marital_status");
+
+            migrationBuilder.DropTable(
+                name: "residential_status");
+        }
+    }
+}
