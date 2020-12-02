3f4a522f34035fbd7b777bc7efe4f123e1f694d4
Georgi Kamacharov <gkamacharov@gmail.com>
2020-12-01 22:03:46 -0500

    add init useful doc

---
+++ aiof.api.data/UsefulDocumentation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace aiof.api.data
+{
+    public class UsefulDocumentation
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public Guid PublicKey { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(100)]
+        public string Page { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Url { get; set; }
+    }
+}
