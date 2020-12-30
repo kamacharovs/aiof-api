669d0ae940bbbeb0be1bb860f54e3c96124cda98
Georgi Kamacharov <gkamacharov@gmail.com>
2020-12-30 17:19:18 -0500

    update default unauthorized and forbidden msg

--- aiof.api.core/AiofUnauthorizedMiddleware.cs
+++ aiof.api.core/AiofUnauthorizedMiddleware.cs
@@ -13,13 +13,13 @@ namespace aiof.api.core
 {
     public class AiofUnauthorizedMiddleware
     {
-    private readonly RequestDelegate _next;
+        private readonly RequestDelegate _next;
 
-        private const string _defaultUnauthorizedMessage = "You don't have access to this API";
-        private const string _defaultForbiddenMessage = "You don't have permission to access this API";
-        private IEnumerable<int> _vallowedStatusCodes = new int[] 
-        { 
-            StatusCodes.Status401Unauthorized, 
+        private const string _defaultUnauthorizedMessage = "Unauthorized. Missing, invalid or expired credentials provided";
+        private const string _defaultForbiddenMessage = "Forbidden. You don't have enough permissions to access this API";
+        private IEnumerable<int> _vallowedStatusCodes = new int[]
+        {
+            StatusCodes.Status401Unauthorized,
             StatusCodes.Status403Forbidden
         };
 
@@ -60,7 +60,7 @@ namespace aiof.api.core
                 .Serialize(aiofProblem, new JsonSerializerOptions { IgnoreNullValues = true });
 
             httpContext.Response.ContentType = Keys.ApplicationProblemJson;
-            
+
             await httpContext.Response
                 .WriteAsync(aiofProblemJson);
         }
