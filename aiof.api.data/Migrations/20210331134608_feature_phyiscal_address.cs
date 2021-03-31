6a2ac8da629ff1fdf02f1096a1c375a2e0873378
Georgi Kamacharov <gkamacharov@gmail.com>
2021-03-31 10:07:18 -0400

    adde feature/phyiscal-address migration

---
+++ aiof.api.data/Migrations/20210331134608_feature_phyiscal_address.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace aiof.api.data.Migrations
+{
+    public partial class feature_phyiscal_address : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "residential_status",
+                table: "user_profile",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "occupation_industry",
+                table: "user_profile",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "occupation",
+                table: "user_profile",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "marital_status",
+                table: "user_profile",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "gender",
+                table: "user_profile",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "education_level",
+                table: "user_profile",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "address",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    public_key = table.Column<Guid>(type: "uuid", nullable: false),
+                    street_line1 = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    street_line2 = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    city = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    state = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: true),
+                    zip_code = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: true),
+                    country = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    user_profile_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_address", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_address_user_profile_user_profile_id",
+                        column: x => x.user_profile_id,
+                        principalTable: "user_profile",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_address_user_profile_id",
+                table: "address",
+                column: "user_profile_id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "address");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "residential_status",
+                table: "user_profile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "occupation_industry",
+                table: "user_profile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "occupation",
+                table: "user_profile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "marital_status",
+                table: "user_profile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "gender",
+                table: "user_profile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "education_level",
+                table: "user_profile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
